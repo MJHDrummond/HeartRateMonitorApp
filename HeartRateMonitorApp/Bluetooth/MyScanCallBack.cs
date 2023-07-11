3cef988985cf3cb40583a00202cce0146d6a8f41
McGregor Drummond <mcgregordrummond@live.co.uk>
2023-07-11 20:29:11 +0200

    New attempt at creating the scan for bluetooth LE

---
+++ HeartRateMonitorApp/Bluetooth/MyScanCallBack.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Bluetooth.LE;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HeartRateMonitorApp.Bluetooth
+{
+    public class MyScanCallback : ScanCallback
+    {
+        private Activity _activityContext;
+
+        public MyScanCallback(Activity activityContext)
+        {
+            this._activityContext = activityContext;
+        }
+
+        public override void OnScanResult(ScanCallbackType callbackType, ScanResult result)
+        {
+            base.OnScanResult(callbackType, result);
+
+            // Handle the scanned BLE device
+            var device = result.Device;
+            var rssi = result.Rssi;
+
+            // Access device information
+            var deviceName = device.Name;
+            var deviceAddress = device.Address;
+
+            // Do something with the scanned device
+            Console.WriteLine($"Found BLE device: {deviceName} ({deviceAddress}), RSSI: {rssi}");
+
+            // Display the scanned device information as a toast
+            //var message = $"Found BLE device: {deviceName} ({deviceAddress}), RSSI: {rssi}";
+            //ShowToastMessage(message);
+
+        }
+        /*
+        private void ShowToastMessage(string message)
+        {
+            _activityContext?.RunOnUiThread(() =>
+            {
+                Toast.MakeText(_activityContext, message, ToastLength.Long).Show();
+            });
+        }
+        */
+
+    }
+}
--- HeartRateMonitorApp/BluetoothActivity.cs
+++ HeartRateMonitorApp/BluetoothActivity.cs
@@ -3,13 +3,10 @@ using Android.Content;
 using Android.OS;
 using Android.Widget;
 using Android.Bluetooth;
+using Android.Runtime;
 using System;
 using HeartRateMonitorApp.Bluetooth;
-//using Android.Bluetooth.LE;
-using Plugin.BLE;
-using Plugin.BLE.Abstractions.Contracts;
-using System.Collections.ObjectModel;
-using Plugin.BLE.Abstractions.Exceptions;
+using Android.Views;
 
 namespace HeartRateMonitorApp
 {
@@ -17,188 +14,119 @@ namespace HeartRateMonitorApp
     public class BluetoothActivity : Activity
     {
         //https://github.com/dotnet-bluetooth-le/dotnet-bluetooth-le
+        
+        private BluetoothAdapter _bluetoothAdapter;
+        private MyScanCallback _scanCallback;
+        private bool _isScanning;
         private Button _mainButton;
-        //private BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-        //private bool scanning;
-        //private Handler handler = new Handler();
-        // Stops scanning after 10 seconds.
-        private static int SCAN_PERIOD = 10000;
+        private Button _scanButton;
+        private Activity _activityContext = (Activity)Application.Context;
 
-        IBluetoothLE _ble;
-        Plugin.BLE.Abstractions.Contracts.IAdapter _adapter;
-        ObservableCollection<IDevice> _deviceList;
-        IDevice _device;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            //SetContentView(Resource.Layout.Main);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_bluetooth);
 
-            FindViews();
-            LinkEventHandlers();
-            /*
-                        //BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter; 
-                        //BluetoothManager bluetoothManager = (BluetoothManager)Context.getSystemService(Context.BLUETOOTH_SERVICE);
-                        BluetoothManager bluetoothManager = (BluetoothManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.BluetoothService);
-                        BluetoothAdapter bluetoothAdapter = bluetoothManager.Adapter
-                        if (adapter == null)
-                            throw new Exception("No Bluetooth adapter found.");
 
-                        if (!adapter.IsEnabled)
-                            throw new Exception("Bluetooth adapter is not enabled.");
-            */
-            _ble = CrossBluetoothLE.Current;
-            _adapter = CrossBluetoothLE.Current.Adapter;
-            _adapter.ScanTimeout = SCAN_PERIOD;
+            // Initialize BluetoothAdapter
+            var bluetoothManager = (BluetoothManager)GetSystemService(Context.BluetoothService);
+            _bluetoothAdapter = bluetoothManager.Adapter;
 
-            var state = _ble.State;
+            // Create the BluetoothLeScanCallback
+            //_scanCallback = new MyBluetoothLeScanCallback();
 
-            _ble.StateChanged += (s, e) =>
+            // Check if BLE is supported on the device
+            if (!_bluetoothAdapter.IsMultipleAdvertisementSupported)
             {
-                //Debug.WriteLine($"The bluetooth state changed to {e.NewState}");
-                //DisplayAlert("Alert", $"The bluetooth state changed to {e.NewState}", "OK");
-                bluetoothStateAlert(s, e);
-            };
+                Console.WriteLine("BLE is not supported on this device");
+                //var message = $"BLE is not supported on this device";
+                //ShowToastMessage(message);
+                return;
+            }
 
-            _deviceList = new ObservableCollection<IDevice>();
-            //bltlist.ItemsSource = deviceList;
+            _scanButton = FindViewById<Button>(Resource.Id.scanButton);
 
-            //scan();
-            //connectToDevice();
+            _scanButton.Click += OnScanButtonClick;
+
+            // Check if Bluetooth is enabled
+            if (!_bluetoothAdapter.IsEnabled)
+            {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+                StartActivityForResult(enableBtIntent, 1);
+            }
+
+            //FindViews();
+            // LinkEventHandlers();
 
 
         }
-
-        private void bluetoothStateAlert(object sender, EventArgs e)
+        private void OnScanButtonClick(object sender, EventArgs e)
         {
-            Android.App.AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-            AlertDialog alert = dialog.Create();
-            alert.SetTitle("The bluetooth state changed");
-            alert.SetMessage($"Info message with current state");
-            alert.SetButton("OK", (c, ev) =>
+            // Start scanning for BLE devices
+            StartScan();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+
+            // Start scanning for BLE devices
+            StartScan();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+
+            // Stop scanning for BLE devices
+            StopScan();
+        }
+
+        private void StartScan()
+        {
+            // Check if already scanning
+            if (_isScanning)
+                return;
+
+            // Check if BluetoothLeScanner is available
+            if (_bluetoothAdapter?.BluetoothLeScanner == null)
             {
-                // Ok button click task  
+                Console.WriteLine("BluetoothLeScanner is not available");
+                //var message = $"BluetoothLeScanner is not available";
+                //ShowToastMessage(message);
+
+                return;
+            }
+
+            // Start scanning for BLE devices
+            _scanCallback = new MyScanCallback(_activityContext);
+            _bluetoothAdapter.BluetoothLeScanner.StartScan(_scanCallback);
+
+            _isScanning = true;
+        }
+
+        private void StopScan()
+        {
+            // Check if scanning
+            if (!_isScanning)
+                return;
+
+            // Stop scanning for BLE devices
+            _bluetoothAdapter.BluetoothLeScanner.StopScan(_scanCallback);
+
+            _isScanning = false;
+        }
+        /*
+        private void ShowToastMessage(string message)
+        {
+            RunOnUiThread(() =>
+            {
+                Toast.MakeText(this, message, ToastLength.Long).Show();
             });
-            alert.Show();
         }
-
-        public async void connectToDevice()
-        {
-            _device = _deviceList[0];
-            try
-            {
-                await _adapter.ConnectToDeviceAsync(_device);
-            }
-            catch (DeviceConnectionException ex)
-            {
-                // ... could not connect to device
-                throw new Exception(message: $"Could not connect to device: {ex.Message.ToString()}");
-            }
-        }
-        public async void scan()
-        {
-
-            //TODO: Check if Bluetooth is on or not, see Enable code below.
-            //TODO: Test with HR band close by
-            try
-            {
-                _deviceList.Clear();
-                _adapter.DeviceDiscovered += (s, a) =>
-                {
-
-                    _deviceList.Add(a.Device);
-                    // DisplayAlert("Disc", a.Device.Id.ToString(), "OK");
-                };
-
-                //We have to test if the device is scanning 
-                if (!_ble.Adapter.IsScanning)
-                {
-                    await _adapter.StartScanningForDevicesAsync();
-                }
-            }
-            catch (Exception ex)
-            {
-                //DisplayAlert("Notice", ex.Message.ToString(), "Error !");
-                throw new Exception(message: ex.Message.ToString());
-            }
-        }
-        /*
-                private void scanLeDevice()
-                {
-                    if (!scanning)
-                    {
-                        // Stops scanning after a predefined scan period.
-                        handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run()
-                            {
-                                scanning = false;
-                                bluetoothLeScanner.stopScan(leScanCallback);
-                            }
-                        }, SCAN_PERIOD);
-
-                    scanning = true;
-                    bluetoothLeScanner.startScan(leScanCallback);
-                    } else {
-                    scanning = false;
-                    bluetoothLeScanner.stopScan(leScanCallback);
-                    }
-                }   
-        */
-        /*
-                protected override void OnResume()
-                {
-                    base.OnResume();
-                    RegisterReceiver(BluetoothDeviceReceiver.Receiver, new IntentFilter(BluetoothDevice.ActionFound));
-                }
-
-                protected override void OnPause()
-                {
-                    base.OnPause();
-                    UnregisterReceiver(BluetoothDeviceReceiver.Receiver);
-                }
-        */
-        /*
-                void Enable(object sender, EventArgs e)
-                {
-                    BluetoothManager _manager;
-                    _manager = (BluetoothManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.BluetoothService);
-                    bool on = _manager.Adapter.IsEnabled;
-                    if (on == true)
-                    {
-                        string error = "Bluetooth is Already on";
-                        Toast.MakeText(this, error, ToastLength.Long).Show();
-                        e.Equals(true);
-                    }
-                    else
-                    {
-                        string okay = "Bluetooth has been turned on";
-                        _manager.Adapter.Enable();
-                        Toast.MakeText(this, okay, ToastLength.Long).Show();
-
-                    }
-                }
-                void Disable(object sender, EventArgs e)
-                {
-                    BluetoothManager _manager;
-                    _manager = (BluetoothManager)Android.App.Application.Context.GetSystemService(Android.Content.Context.BluetoothService);
-                    bool on = _manager.Adapter.IsEnabled;
-                    if (on == false)
-                    {
-                        string error = "Bluetooth is Already off";
-                        Toast.MakeText(this, error, ToastLength.Long).Show();
-                        e.Equals(false);
-
-                    }
-                    else
-                    {
-                        string okay = "Bluetooth has been turned off";
-                        _manager.Adapter.Disable();
-                        Toast.MakeText(this, okay, ToastLength.Long).Show();
-
-                    }
-                }
         */
         private void LinkEventHandlers()
         {
@@ -207,10 +135,10 @@ namespace HeartRateMonitorApp
 
         private void _mainButton_Click(object sender, EventArgs e)
         {
-            //Intent intent = new Intent(this, typeof(MainActivity));
-            //StartActivity(intent);
-            scan();
-            connectToDevice();
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            //scan();
+            //connectToDevice();
             //BluetoothAdapter.DefaultAdapter.StartDiscovery();
 
         }
